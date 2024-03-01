cb14b791bad67ea8902dc4d9591b5e9cd9c97516
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2024-02-29 16:03:14 -0800

    practice

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-fn main(){
-    const SECONDS_IN_MINUTE: u32 = 60;
-
-    println!("{}",SECONDS_IN_MINUTE)
+use std::io;
 
 
-}
+
+
+
+//Input function that takes input as int
+// fn main(){
+//     let mut input = String::new();
+
+//     println!("Enter an integer:");
+//     io::stdin().read_line(&mut input).expect("Failed to read line");
+
+//     let num: i32 = input.trim().parse().expect("Invalid input"); //here this line is using iu32 to recognize input as integer. 
+//     println!("{}",num);
+
+// }
